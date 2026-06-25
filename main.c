63f9cce49256d51199a7830e158061e7054fd367
kosuke <kajitakosuke@gmail.com>
2026-06-25 17:05:15 +0900

    branch merged

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(){
     int count=1;
     printf("C make practice\n");
     printf("count= %d\n",count);
-    /print_message();
+    print_message();
     count=count+1;
     printf("count=%d\n",count);
     printf("add branch");
